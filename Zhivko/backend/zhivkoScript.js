ac40dcc201e4cf9acb5daf210521a9749547dedc
JustZhivko <jivko_u@abv.bg>
2025-05-24 11:00:04 +0300

    Created product page with filtering by categories

---
+++ Zhivko/backend/zhivkoScript.js
@@ -0,0 +1,121 @@
+let products = [
+    {
+        name: 'UNCHARTED 4',
+        price: '19.55',
+        image: '../frontend/images/28402.jpg',
+        filter: 'Adventure',
+    },
+    {
+        name: 'Microsoft Flight Simulator 40th Anniversary Edition',
+        price: '69.99',
+        image: '../frontend/images/capsule_616x353.jpg',
+        filter: 'Simulator',
+    },
+    {
+        name: 'Cyberpunk 2077',
+        price: '23.99',
+        image: '../frontend/images/cyberpunk-2077-button-fin-1594877291453.jpg',
+        filter: 'RPG games',
+    },
+    {
+        name: 'Hello Neighbor',
+        price: '28.99',
+        image: '../frontend/images/header (1).jpg',
+        filter: 'Adventure',
+    },
+    {
+        name: 'Euro Truck Simulator 2',
+        price: '4.99',
+        image: '../frontend/images/header (2).jpg',
+        filter: 'Simulator',
+    },
+    {
+        name: 'X-Plane 12',
+        price: '58.99',
+        image: '../frontend/images/header (3).jpg',
+        filter: 'Simulator',
+    },
+    {
+        name: 'NBA 2K25',
+        price: '69.99',
+        image: '../frontend/images/header (4).jpg',
+        filter: 'Sport',
+    },
+    {
+        name: 'EA SPORTS FC™ 25',
+        price: '69.99',
+        image: '../frontend/images/header (5).jpg',
+        filter: 'Sport',
+    },
+    {
+        name: 'Portal',
+        price: '9.75',
+        image: '../frontend/images/header.jpg',
+        filter: 'Adventure',
+    },
+    {
+        name: 'Gaometry Dash',
+        price: '4.99',
+        image: '../frontend/images/Logo_of_Geometry_Dash.png',
+        filter: 'Action',
+    },
+    {
+        name: 'Sonic Mania',
+        price: '19.99',
+        image: '../frontend/images/MV5BMWJlZTRkZmEtZjA1OS00YzYwLTlhYTAtZmJjYWYxYWJhOTFjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+        filter: 'Action',
+    },
+    {
+        name: 'Need for Speed™ Payback',
+        price: '29.99',
+        image: '../frontend/images/Need_for_Speed_Payback_standard_edition_cover_art.jpg',
+        filter: 'Racing',
+    },
+    
+];
+
+
+render();
+
+
+function render(){
+  let HTML = '';
+  for(let i=0 ; i<products.length ; i++){
+    let html = `
+    <div class="game-card">
+              <img src="${products[i].image}" alt="Game ${i}">
+              <div class="game-info">
+                  <h3>${products[i].name}</h3>
+                  <p class="price">$${products[i].price}</p>
+                  <button class="buy-btn">Buy Now</button>
+              </div>
+          </div>
+    `;
+    
+    HTML += html;
+  
+  }
+  document.querySelector('.games-grid').innerHTML=HTML; 
+}
+
+function filter(filter){
+  let HTML = '';
+  for(let i=0; i<products.length ; i++){
+    if(products[i].filter===filter){
+    let html = `
+    <div class="game-card">
+              <img src="${products[i].image}" alt="Game ${i}">
+              <div class="game-info">
+                  <h3>${products[i].name}</h3>
+                  <p class="price">$${products[i].price}</p>
+                  <button class="buy-btn">Buy Now</button>
+              </div>
+          </div>
+    `;
+    
+    HTML += html;
+  }
+  }
+  document.querySelector('.games-grid').innerHTML=HTML;
+  document.querySelector('.section-title').innerHTML=filter;
+}
